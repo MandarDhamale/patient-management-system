eace7e8a86bbfa87f408ce39cd94f2b04d95f207
Mandar Dhamale <mandardhamale@gmail.com>
2025-12-28 17:48:02 -0500

    patient create and update validation

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -1,16 +1,17 @@
 package com.pm.patientservice.controller;
 
+import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
-import com.pm.patientservice.mapper.PatientMapper;
-import com.pm.patientservice.model.Patient;
-import com.pm.patientservice.repository.PatientRepository;
+import com.pm.patientservice.dto.validators.CreatePatientValidationGroup;
 import com.pm.patientservice.service.PatientService;
+import jakarta.validation.Valid;
+import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
@@ -20,9 +21,24 @@ public class PatientController {
     private PatientService patientService;
     public PatientController(PatientService patientService) {this.patientService = patientService;}
 
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients(){
-        List<PatientResponseDTO> patientResponseDTOS = patientService.getPatients();
-        return ResponseEntity.ok().body(patientResponseDTOS);
+        List<PatientResponseDTO> patientResponseDTOs = patientService.getPatients();
+        return ResponseEntity.ok().body(patientResponseDTOs);
     }
-}
+
+    @PostMapping
+    public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO){
+        PatientResponseDTO patientResponseDTO = patientService.createPatient(patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable UUID id, @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO){
+
+        PatientResponseDTO patientResponseDTO = patientService.updatePatient(id, patientRequestDTO);
+        return ResponseEntity.ok().body(patientResponseDTO);
+
+    }
+
+ }
