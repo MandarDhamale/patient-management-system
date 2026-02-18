6291ede6cd97fc16493985fdea31a258329f18da
Mandar Dhamale <mandardhamale@gmail.com>
2026-02-18 15:25:19 -0500

    token generation logic

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.pm.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,14 @@ public class AuthController  {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         String token = tokenOptional.get();
+        System.out.println(loginRequestDTO.getEmail());
+        System.out.println(loginRequestDTO.getPassword());
         return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
     }
 
 
