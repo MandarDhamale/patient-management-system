58f8bc5d39b668d143b08f7c7ef213124dd989d8
Mandar Dhamale <mandardhamale@gmail.com>
2026-03-17 17:23:15 -0400

    Patient Integration Test

---
+++ integration-tests/src/test/java/PatientIntegrationTest.java
@@ -0,0 +1,38 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class PatientIntegrationTest {
+
+    @BeforeAll
+    public static void setup(){
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    @Test
+    public void shouldReturnPatientsWithValidToken(){
+
+        String loginPayload = """
+                    {
+                        "email": "testuser@test.com",
+                        "password": "password123"
+                    }
+                """;
+
+        String token = given().contentType("application/json").body(loginPayload).when().post("/auth/login").then().statusCode(200).extract().jsonPath().get("token"); // extract method is going to pull the response from the request that we made up above and .response() is going to get the response object and assign to the response variable
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/api/patients")
+                .then()
+                .statusCode(200)
+                .body("patients", notNullValue());
+    }
+
+
+}
